bb8a620f96311b9545f611e39f869922a6536441
FX35RUS <50159251+FX35RUS@users.noreply.github.com>
2020-01-02 19:54:14 +0300

    отскок назад
    проба отскока

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ $(document).ready(function() {
   paper.setup(document.getElementById("mainCanvas"));
 
   //TODO
+<<<<<<< HEAD
  
   var colorCircle = ["red", "green", "blue", "black"];
+=======
+  /*var c=Shape.Circle(0,200,50);
+    c.fillColor="green";*/
+
+  /*var c;
+    for (let x = 25; x < 400; x+=50) {
+        for (let y = 25; y < 400; y+=50) {
+            c=Shape.Circle(x,y,20);
+            c.fillColor="green";
+        }
+    }*/
+
+  var colorCircle=["red","green","blue","black"];
+>>>>>>> parent of 4a7605b... Update main.js
   var fillColor;
-  var i = 0;
+  var i=0;
   var tool = new Tool();
   var c = Shape.Circle(200, 200, 80);
   c.fillColor = "black";
@@ -19,14 +34,20 @@ $(document).ready(function() {
 
   tool.onMouseDown = function(event) {
     var c = Shape.Circle(event.point, 20);
-
-    fillColor = colorCircle[i];
+      
+    fillColor=colorCircle[i]
+    console.log(i+" "+fillColor+" начало обработки");
     c.fillColor = fillColor;
-    if (i < colorCircle.length - 1) i++;
-    else i = 0;
+
+    if (i<colorCircle.length-1) {
+      i++;
+    }   
+    else i=0;
+    console.log(i+" "+fillColor+" конец обработки");
   };
 
   paper.view.draw();
+  console.log("main.js loaded");
 });
 
-
+console.log("main.js loaded");
